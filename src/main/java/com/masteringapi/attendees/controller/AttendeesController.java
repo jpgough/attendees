35cfed24e61f9dc9aaabdadc393862f7238f7ae9
James Gough <jpgough@gmail.com>
2022-03-23 13:24:48 +0000

    Add an external URL in

--- src/main/java/com/masteringapi/attendees/controller/AttendeesController.java
+++ src/main/java/com/masteringapi/attendees/controller/AttendeesController.java
@@ -4,9 +4,11 @@ import com.masteringapi.attendees.model.AttendeeResponse;
 import com.masteringapi.attendees.service.AttendeeStore;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class AttendeesController {
@@ -14,11 +16,21 @@ public class AttendeesController {
     @Autowired
     private AttendeeStore store;
 
+    private RestTemplate restTemplate = new RestTemplate();
+
     @GetMapping("/attendees")
     @ResponseBody
     @ApiOperation(value = "Retrieve a list of all attendees stored within the system", nickname = "getAttendees")
     public AttendeeResponse getAttendees() {
         return new AttendeeResponse(store.getAttendees());
     }
+
+    @GetMapping("/external")
+    @ResponseBody
+    public String getExternalContent() {
+        ResponseEntity<String> entity = restTemplate.getForEntity("http://52.55.211.119/get", String.class);
+        return entity.toString();
+    }
+
 
 }
